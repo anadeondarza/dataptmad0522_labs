4ae9ae5e5130ceafa416f8af7b7a5b56c8055de6
adeondarza@gmail.com <anadeondarza@MacBook-Pro-de-Ana.local>
2022-07-18 19:43:59 +0200

    casi

--- Proyecto_Final/acquisition/acquisition.py
+++ Proyecto_Final/acquisition/acquisition.py
@@ -1,10 +1,16 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
 import pandas as pd
 import requests
+from shapely.geometry import Point
+import geopandas as gpd 
+
 
 def acquisition(path):
     response = requests.get(path)
     jsons = response.json()
-    return(json)
+    return(jsons)
 
 def acquisition2(path):
     engine = create_engine(connect_str)
